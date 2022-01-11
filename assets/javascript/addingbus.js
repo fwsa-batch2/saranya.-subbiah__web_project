1245d99ebab3b208f253e2967300743d2e2c3e37
Saran-20 <Saranyaselv2820@gmail.com>
2022-01-11 12:26:02 +0530

    add

--- assets/javascript/addingbus.js
+++ assets/javascript/addingbus.js
@@ -2,7 +2,7 @@ let customername = [];
 function PageOnLoad() {
     let buses = JSON.parse(localStorage.getItem("source and destination"));
     if (buses != null) {
-     let list = buses;
+      list = buses;
     }
 }
  
